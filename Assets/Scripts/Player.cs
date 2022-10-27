3049787163ea6748ee7ba6ee0852e03940c5fb31
Junsee <dat10319@gmail.com>
2022-10-27 17:59:46 +0700

    Create Animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,47 +5,104 @@ public class Player : MonoBehaviour
     Rigidbody2D rb;
     Animator animator;
     SpriteRenderer sprite;
+    CapsuleCollider2D coll;
 
     public float moveSpeed = 5f;
     public float jumpForced = 8f;
     public float dodgeSpeed = 8f;
-    float direction = 0f;
+    float directionX = 0f;
+    float directionY = 0f;
     
     public float groundCheckRadius = 0f;
     public LayerMask groundMask;
     public Transform groundCheck;
     bool isTouchingGround;
 
+    bool isDodge;
+    bool flipX;
+    
+    bool isActack;
+    int noOfActack = 0;
+    // int stateActack = 0;
+    float lastActackTime = 0f;
+    public float maxComboDelay = 0.9f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
+        coll = GetComponent<CapsuleCollider2D>();
     }
 
     void Update()
     {
         isTouchingGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundMask);
-        direction = Input.GetAxisRaw("Horizontal");
+        isDodge = Input.GetButtonDown("Fire1");
+        isActack = Input.GetButtonDown("Fire2");
+        if (Time.time - lastActackTime > maxComboDelay)
+        {
+            noOfActack = 0;
+        }
 
-        rb.velocity = new Vector2(direction * moveSpeed, rb.velocity.y);
-        switch (direction)
+        directionX = Input.GetAxisRaw("Horizontal");
+        directionY = Input.GetAxisRaw("Vertical");
+
+        rb.velocity = new Vector2(directionX * moveSpeed, rb.velocity.y);
+
+        if (isActack)
+        {
+            PlayerActack();
+        }
+        
+        PlayerAnimator();
+        PlayerDodge();
+        PlayerMoving();
+    }
+    void PlayerMoving()
+    {
+        switch (directionX)
         {
             case 1:
-                sprite.flipX = false;
+                flipX = false;
                 break;
             case -1:
-                sprite.flipX = true;
+                flipX = true;
                 break;
         }
-        if (Input.GetButtonDown("Jump") && isTouchingGround)
+
+        if (directionY > 0 && isTouchingGround)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForced);
         }
-
+        else if (directionY < 0 && !isTouchingGround && isActack)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, -jumpForced);
+            animator.SetTrigger("Actack");
+        }
+    }
+    public void PlayerDodge()
+    {
+        // int direX = (flipX ? -1 : 1);
+        // rb.velocity = new Vector2(direX * dodgeSpeed, rb.velocity.y);
+    }
+    void PlayerAnimator()
+    {
+        sprite.flipX = flipX;
         animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
         animator.SetFloat("OnAir", rb.velocity.y);
         animator.SetBool("OnGround", isTouchingGround);
-        animator.SetBool("Dodge", false);
+        animator.SetBool("Dodge", isDodge);
+        animator.SetInteger("ActackState", noOfActack);
+
+    }
+    void PlayerActack()
+    {   
+        lastActackTime = Time.time;
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Fall") && !animator.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
+        {
+            noOfActack = (noOfActack >= 3 ? noOfActack = 1 : noOfActack + 1);
+        }
+        animator.SetTrigger("Actack");
     }
 }
